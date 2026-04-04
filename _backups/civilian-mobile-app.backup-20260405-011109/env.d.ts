7eb52a74dd3e4a5c8c7bf7a8193f8d7bbefa9d1b
bert-dev13 <ilacengelbert13@gmail.com>
2026-04-05 03:43:09 +0800

    fix civilian app

--- _backups/civilian-mobile-app.backup-20260405-011109/env.d.ts
+++
@@ -1,8 +0,0 @@
-declare namespace NodeJS {
-  interface ProcessEnv {
-    EXPO_PUBLIC_LOGS_ENDPOINT?: string;
-    EXPO_PUBLIC_PROJECT_GROUP_ID?: string;
-    EXPO_PUBLIC_CREATE_TEMP_API_KEY?: string;
-    EXPO_PUBLIC_DEV_SERVER_ID?: string;
-  }
-}
